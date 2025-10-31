c210165e169362551fbfb14d8a6fd3faf2d458c4
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-10-31 17:09:27 +0200

    Add Micromodal dependency and implement modal functionality; remove unused component HTML file

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,18 @@
 import "../styles/main.scss";
 import AccordionCollection from "./Accordion";
+import Micromodal from "micromodal";
 
 document.addEventListener("DOMContentLoaded", (): void => {
   new AccordionCollection();
+});
+
+const popups = document.querySelectorAll("[data-custom-popup]");
+popups.forEach(popup => {
+  const popupElement = popup as HTMLElement;
+  if (popupElement?.id) {
+    const delay = parseInt(popupElement?.dataset?.delay || "1000");
+    setTimeout(() => {
+      Micromodal.show(popupElement?.id);
+    }, delay);
+  }
 });
